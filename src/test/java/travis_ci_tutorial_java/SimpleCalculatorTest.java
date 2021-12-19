18355c5a2c638da0072d74b5807f120395febf32
jayoung60 <90717906+jayoung60@users.noreply.github.com>
2021-12-19 14:56:41 -0500

    FixedErrors
    fixed errors and added subtract, multiply, and divide (which doesn't error when dividing by 0)

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -9,6 +9,22 @@ public class SimpleCalculatorTest {
 	@Test
 	public void testAdd() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.add(1, 1), 0);
+		assertEquals(calc.add(1, 1), 2);
+	}
+	@Test
+	public void testSubtract() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.subtract(1, 1), 0);
+	}
+	@Test
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(3, 2), 6);
+	}
+	@Test
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(4, 2), 2);
+		assertEquals(calc.divide(4, 0), 0);
 	}
 }
